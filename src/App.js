65f3dd106c133a95012484dc0416ba5c332e5953
flin1105 <flin1105@DESKTOP-BFV96NP.localdomain>
2021-07-01 17:40:43 -0400

    Update repo july1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Dashboard from './components/Dashboard'
 import NavBar from './components/NavBar'
 import TestQuery from './components/Test-Query'
+import Header from './components/Header'
+import Playground from './components/Playground';
 import './stylesheets/index.css';
 
 import {
@@ -12,15 +14,25 @@ import {
 function App() {
   return (
     <div id="App">
-      <h1>Hello Worldddddd</h1>
+      {/* <h1>Hello Worldddddd</h1> */}
+      <Header />
       <Router>
         <NavBar />
         <Switch>
+          <Route exact path="/">
+            <div id='landing'>
+              <h1>Welcome back, beautiful!</h1>
+              <h3>Click a tab to get started...</h3>
+            </div>
+          </Route>
           <Route path="/dashboard">
             <Dashboard />
           </Route>
           <Route path="/testquery">
             <TestQuery />
+          </Route>
+          <Route path="/playground">
+            <Playground />
           </Route>
         </Switch>
       </Router>
