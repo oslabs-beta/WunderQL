17c5e5ed3c7b244f95ea45190d1322e159b6bd1c
pziggy3 <pziegler9@gmail.com>
2021-07-07 16:39:31 -0400

    set up sql queries

---
+++ wunderql_postgres.sql
@@ -0,0 +1,47 @@
+CREATE TABLE public.users (
+	"_id" serial PRIMARY KEY,
+	"username" varchar UNIQUE,
+	"password" varchar
+) WITH (
+  OIDS=FALSE
+);
+
+
+CREATE TABLE public.projects (
+	"_id" serial PRIMARY KEY,
+	"project_name" varchar,
+	"user_id" bigint,
+	FOREIGN KEY (user_id) REFERENCES users (_id)
+) WITH (
+  OIDS=FALSE
+);
+
+
+CREATE TABLE public.graphqlurls (
+	"_id" serial PRIMARY KEY,
+	"url" varchar,
+	"project_id" bigint,
+	FOREIGN KEY (project_id) REFERENCES projects (_id)
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE public.queries (
+	"_id" serial PRIMARY KEY,
+	"query_string" varchar,
+	"url_id" bigint,
+	FOREIGN KEY (url_id) REFERENCES graphqlurl (_id)
+) WITH (
+  OIDS=FALSE
+);
+
+CREATE TABLE public.response_times (
+	"_id" serial PRIMARY KEY,
+  "date" varchar,
+  "response_time" varchar,
+	"query_id" bigint,
+	FOREIGN KEY (query_id) REFERENCES queries (_id)
+) WITH (
+  OIDS=FALSE
+);
+
