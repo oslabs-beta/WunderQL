26acb68d60b3c8423828253093d339a2979fcf97
Raubern Totanes <rstotanes@g.ucla.edu>
2021-07-08 01:01:12 -0400

    improve line chart component

---
+++ src/components/CustomTooltip.jsx
@@ -0,0 +1,5 @@
+const CustomTooltip = ({ date }) => {
+  return <div>date</div>
+}
+
+export default CustomTooltip;
--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,4 +1,5 @@
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { ScatterChart, LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import CustomTooltip from './CustomTooltip';
 
 const LineChartComponent = ({ history }) => {
   const data = [];
@@ -11,18 +12,23 @@ const LineChartComponent = ({ history }) => {
   // for (let i = 0; i < 20; i++) sample.push((Math.random()*3 + 148).toFixed(2));
 
   // create data points for each number in sample array
-  if (history) {
-    console.log('hi im history from the line chart component: ', history)
-    history.map((obj, index) => data.push({day: obj.date, runtime: obj.runtime}));
-    // history.map((obj, index) => data.push({day: index, runtime: obj.runtime}));
-  }
+  // if (history) {
+    
+  //   console.log('hi im history from the line chart component: ', history)
+  //   history.forEach((obj, index) => {
+  //     data.push({index: index, day: obj.date, runtime: obj.runtime})
+  //   });
+  //   // history.map((obj, index) => data.push({day: index, runtime: obj.runtime}));
+  // }
   console.log('data to be plotted: ', data)
+  // const date = history.date;
+  // const payloadFormatter = (value, name, props) => (['date', 'date2']);
 
   return (
     <ResponsiveContainer width='100%' height='100%'>
       <LineChart
             id='line-chart'
-            data={data}
+            data={history}
             // data={props.data}
             margin={{
               top: 5,
@@ -33,7 +39,7 @@ const LineChartComponent = ({ history }) => {
             >
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis 
-          dataKey="day" 
+          dataKey="index" 
           // domain={['dataMax - 20', 'dataMax']}
         />
         <YAxis 
@@ -41,9 +47,15 @@ const LineChartComponent = ({ history }) => {
           domain={['dataMin - 10', 'dataMax + 10']} 
           allowDecimals='false'
         />
-        <Tooltip />
+        <Tooltip 
+          // payload="[{day: 'date'}]"
+          // formatter={payloadFormatter}
+          // label={date}
+          // content={<CustomTooltip date={history.date} />}
+        />
         {/* <Legend /> */}
-        <Line type="monotone" dataKey="runtime" stroke="#8884d8" activeDot={{ r: 8 }} />
+        <Line type="monotone" dataKey="runtime" dot={{stroke: 'red', strokeWidth: 1}} strokeWidth={0}activeDot={{ r: 8 }} />
+        <Line type="monotone" dataKey="best_fit" dot={false} stroke="#8884d8" />
       </LineChart>
     </ResponsiveContainer>
   )
