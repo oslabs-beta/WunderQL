3f6ded9b82c41c1939a028454ce6581644c56cd5
ldllano <69124330+ldllano@users.noreply.github.com>
2021-07-16 14:17:48 -0400

    Update database structure

--- wunderql_postgres.sql
+++ wunderql_postgres.sql
@@ -1,47 +1,51 @@
 CREATE TABLE public.users (
-	"_id" serial PRIMARY KEY,
-	"username" varchar UNIQUE,
-	"password" varchar
+  "_id" serial PRIMARY KEY,
+  "name" varchar,
+  "email" varchar,
+  "username" varchar UNIQUE,
+  "password" varchar
 ) WITH (
   OIDS=FALSE
 );
-
-
-CREATE TABLE public.projects (
-	"_id" serial PRIMARY KEY,
-	"project_name" varchar,
-	"user_id" bigint,
-	FOREIGN KEY (user_id) REFERENCES users (_id)
-) WITH (
-  OIDS=FALSE
-);
-
 
 CREATE TABLE public.graphqlurls (
-	"_id" serial PRIMARY KEY,
-	"url" varchar,
-	"project_id" bigint,
-	FOREIGN KEY (project_id) REFERENCES projects (_id)
+  "_id" serial PRIMARY KEY,
+  "nickname" varchar,
+  "url" varchar,
+  "user_id" bigint,
+  FOREIGN KEY (user_id) REFERENCES users (_id)
 ) WITH (
   OIDS=FALSE
 );
 
 CREATE TABLE public.queries (
-	"_id" serial PRIMARY KEY,
-	"query_string" varchar,
-	"url_id" bigint,
-	FOREIGN KEY (url_id) REFERENCES graphqlurls (_id)
+  "_id" serial PRIMARY KEY,
+  "query_name" varchar,
+  "query_string" varchar,
+  "url_id" bigint,
+  FOREIGN KEY (url_id) REFERENCES graphqlurls (_id)
 ) WITH (
   OIDS=FALSE
 );
 
 CREATE TABLE public.response_times (
-	"_id" serial PRIMARY KEY,
-  "date" bigint,
+  "_id" serial PRIMARY KEY,
+  "date" varchar,
   "response_time" float,
-	"query_id" bigint,
-	FOREIGN KEY (query_id) REFERENCES queries (_id)
+  "query_id" bigint,
+  FOREIGN KEY (query_id) REFERENCES queries (_id)
 ) WITH (
   OIDS=FALSE
 );
 
+CREATE TABLE public.load_test_response_times (
+  "_id" serial PRIMARY KEY,
+  "date" varchar,
+  "number_of_child_processes" bigint,
+  "average_response_time" float,
+  "result" varchar,
+  "query_id" bigint,
+  FOREIGN KEY (query_id) REFERENCES queries (_id)
+) WITH (
+  OIDS=FALSE
+);
