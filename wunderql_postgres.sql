3db9510b558568b3ba5e5e020bae44413a1c2bac
ldllano <69124330+ldllano@users.noreply.github.com>
2021-07-19 12:08:35 -0400

    update SQL table constraints

--- wunderql_postgres.sql
+++ wunderql_postgres.sql
@@ -1,18 +1,19 @@
 CREATE TABLE public.users (
   "_id" serial PRIMARY KEY,
-  "name" varchar,
-  "email" varchar,
-  "username" varchar UNIQUE,
-  "password" varchar
+  "name" varchar NOT NULL,
+  "email" varchar UNIQUE NOT NULL,
+  "username" varchar UNIQUE NOT NULL,
+  "password" varchar NOT NULL
 ) WITH (
   OIDS=FALSE
 );
 
 CREATE TABLE public.graphqlurls (
   "_id" serial PRIMARY KEY,
-  "nickname" varchar,
-  "url" varchar,
-  "user_id" bigint,
+  "nickname" varchar NOT NULL,
+  "url" varchar NOT NULL,
+  "user_id" bigint NOT NULL,
+  UNIQUE (url, user_id)
   FOREIGN KEY (user_id) REFERENCES users (_id)
 ) WITH (
   OIDS=FALSE
@@ -20,9 +21,10 @@ CREATE TABLE public.graphqlurls (
 
 CREATE TABLE public.queries (
   "_id" serial PRIMARY KEY,
-  "query_name" varchar,
-  "query_string" varchar,
-  "url_id" bigint,
+  "query_name" varchar NOT NULL,
+  "query_string" varchar NOT NULL,
+  "url_id" bigint NOT NULL,
+  UNIQUE (query_string, url_id)
   FOREIGN KEY (url_id) REFERENCES graphqlurls (_id)
 ) WITH (
   OIDS=FALSE
@@ -30,9 +32,9 @@ CREATE TABLE public.queries (
 
 CREATE TABLE public.response_times (
   "_id" serial PRIMARY KEY,
-  "date" varchar,
-  "response_time" float,
-  "query_id" bigint,
+  "date" varchar NOT NULL,
+  "response_time" float NOT NULL,
+  "query_id" bigint NOT NULL,
   FOREIGN KEY (query_id) REFERENCES queries (_id)
 ) WITH (
   OIDS=FALSE
@@ -40,12 +42,36 @@ CREATE TABLE public.response_times (
 
 CREATE TABLE public.load_test_response_times (
   "_id" serial PRIMARY KEY,
-  "date" varchar,
-  "number_of_child_processes" bigint,
-  "average_response_time" float,
-  "result" varchar,
-  "query_id" bigint,
+  "date" varchar NOT NULL,
+  "number_of_child_processes" bigint NOT NULL,
+  "average_response_time" float NOT NULL,
+  "result" varchar NOT NULL,
+  "query_id" bigint NOT NULL,
   FOREIGN KEY (query_id) REFERENCES queries (_id)
 ) WITH (
   OIDS=FALSE
 );
+
+insert into users(name, email, username, password) values('frank', 'frank@codesmith.com', 'frank', '123')
+
+INSERT INTO graphqlurls(nickname, url, user_id) VALUES ('my_first_endpoint', 'https://api.spacex.land/graphql/', 1) returning _id
+
+INSERT INTO queries(query_name, query_string, url_id) VALUES ('my_first_query','query {
+  launchesPast(limit: 10) {
+      mission_name
+      launch_date_local
+      launch_site {
+          site_name_long
+        }
+      }
+    }', 1)
+
+INSERT INTO response_times(response_time, query_id, date) VALUES (200.5, 1, '2021-07-17T20:25:48.261Z')
+
+INSERT INTO load_test_response_times (date, number_of_child_processes, average_response_time, result, query_id) VALUES('2021-07-17T20:23:12.820Z', 4, 300.5, 'Success', 1)
+
+
+
+  
+
+  
