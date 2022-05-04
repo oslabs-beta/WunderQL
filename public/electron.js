242a3cc4582f31c487896a53a436c92721d5f5d1
Laura <ldllano@gmail.com>
2022-05-04 11:23:26 -0400

    remove comments from main

--- public/electron.js
+++ public/electron.js
@@ -1,5 +1,3 @@
-// const { fetch } = require('cross-fetch');
-// const { performance } = require('perf_hooks');
 const path = require('path');
 const { app, BrowserWindow, Menu, ipcMain } = require('electron');
 const isDev = require('electron-is-dev');
@@ -7,9 +5,9 @@ const { checkResponseTime, loadTest, checkIfQueryExist } = require('./utils');
 const db = require('../models/User');
 const url = require('url');
 
-//-------Electron Setup--------------------------------------------------------
+
 function createWindow() {
-  // Create the browser window.
+
   const win = new BrowserWindow({
     width: 1000,
     height: 700,
@@ -24,28 +22,11 @@ function createWindow() {
       contextIsolation: true,
       enableRemoteModule: false,
       preload: path.join(__dirname, 'preload.js'),
-      /* eng-disable PRELOAD_JS_CHECK */
       disableBlinkFeatures: 'Auxclick'
     },
   });
 
   win.setResizable(true);
-
-  // and load the index.html of the app.
-  // win.loadURL(
-  //   isDev
-  //     ? 'http://localhost:3000'
-  //     :  path.join(__dirname, 'dist', 'index.html')
-  // );
- 
-  // win.loadFile(path.join(__dirname, 'index.html'));
-  // win.loadURL(url.format({
-  //   pathname: path.join(__dirname, 'index.html'),
-  //   protocol: 'file:',
-  //   slashes: true
-  // }));
-  // console.log(path.join(__dirname, 'index.html'))
-  // Quit app when closed; closes all children windows too
 
   const indexPath = url.format({
     protocol: 'file:',
@@ -60,28 +41,22 @@ function createWindow() {
     app.quit();
   });
   
-  // Build menu from template
-  // const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
-
-  // Menu.setApplicationMenu([]);
-  // Menu.setApplicationMenu(mainMenu);
-  // Open the DevTools.
+  
   if (isDev) {
     win.webContents.openDevTools({ mode: 'detach' });
   }
 
 }
 
-// Create menu template
 const mainMenuTemplate =  [
-  // Each object is a dropdown
+ 
   {
     label: 'Raubern',
     submenu:[
       {
         label:'Add Item',
         click(){
-          // createAddWindow();
+
         }
       },
       {
@@ -102,12 +77,11 @@ const mainMenuTemplate =  [
 
 ];
 
-// This method will be called when Electron has finished initialization and is ready to create browser windows.
-// app.whenReady().then(createWindow);
+
 app.on('ready', createWindow);
 
 
-// Quit when all windows are closed, except on macOS. There, it's common for applications and their menu bar to stay active until the user quits
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -115,18 +89,16 @@ app.on('window-all-closed', () => {
 });
 
 ipcMain.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
   }
 });
 
-//-------------------------------------------------------------------------------
+
 
 
 ipcMain.on('signUpToMain', async (event, arg) => {
-  //Get Users Name and Password from Login Form
+
   let doesUserExist = false;
   console.log('from electron.js function signUpToMain', arg);
   try {
@@ -136,7 +108,7 @@ ipcMain.on('signUpToMain', async (event, arg) => {
       values: [arg.username, arg.email],
     };
 
-    // Check to see if username or email already exists in database
+
     const usersExist = await db.query(doesUserExistsQuery);
     if(usersExist.rows.length){
       doesUserExist= true;
@@ -148,7 +120,6 @@ ipcMain.on('signUpToMain', async (event, arg) => {
       };
 
       const newUserID = await db.query(createNewUserQuery);
-      //do I need to send back to front end?
       doesUserExist = true;
       event.sender.send('fromMainSignup', doesUserExist);
       console.log('newUserID from electron.js', newUserID.rows[0]._id);
@@ -165,7 +136,7 @@ ipcMain.on('signUpToMain', async (event, arg) => {
 
 //! #1 loginToMain - User logins 
 ipcMain.on('loginToMain', async (event, arg) => {
-  //Get Users Name and Password from Login Form
+
   try {
     let userId;
     const validateUserQuery = {
@@ -173,18 +144,17 @@ ipcMain.on('loginToMain', async (event, arg) => {
       values: [arg.username, arg.password],
     };
 
-    //Check to see if valid username and password combination exists
+
     const validUsers = await db.query(validateUserQuery);
-    // console.log(validUsers);
+
     if(validUsers.rows.length){
       userId=validUsers.rows[0]._id;
       event.sender.send('userLoggedInFromMain', true);
-      // Gets sent to App.js
       event.sender.send('userIdFromMain', userId);
     } 
 
     console.log(userId);
-    // should the following go inside the above conditional?
+ 
     const getUrlsQuery = {
       text: 'SELECT _id, url, nickname FROM graphqlurls WHERE user_id = $1',
       values: [userId]
@@ -202,10 +172,7 @@ ipcMain.on('loginToMain', async (event, arg) => {
 //! #2 urlToMain - User selects a URL
 ipcMain.on('urlToMain', async (event, arg) => {
   try {
-  
-    //* * ///////////////////////////////////////////////////////////////////////////////////////////////
-    //* * Check if URL Exists and Insert it not
-    //check if the url exists 
+
     const checkIfUrlExists = {
       text: 'SELECT * FROM graphqlurls WHERE url = $1 AND user_id = $2',
       values: [arg.url, arg.userID],
@@ -223,11 +190,9 @@ ipcMain.on('urlToMain', async (event, arg) => {
     } else {
       urlId = existingUrlId[0]._id;
     }
-    // Send id  back to Home.jsx 
-    event.sender.send('idFromMain', urlId);
-    //* * ///////////////////////////////////////////////////////////////////////////////////////////////
 
-    // get all queries for specific url (we have id from above)
+    event.sender.send('idFromMain', urlId);
+
     const getQueriesQuery = {
       text: 'SELECT _id, query_string, query_name FROM queries WHERE url_id = $1',
       values: [urlId]
@@ -237,14 +202,6 @@ ipcMain.on('urlToMain', async (event, arg) => {
     console.log('allQueries', allQueries);
     event.sender.send('queriesFromMain', allQueries);
 
-    // // Send urls to Home.jsx so the options can update
-    // const getUrlsQuery = {
-    //   text: 'SELECT _id, url, nickname FROM graphqlurls WHERE user_id = $1',
-    //   values: [arg.userId]
-    // }
-    // const urlsResult = await db.query(getUrlsQuery);
-    // const results = urlsResult.rows;
-    // event.sender.send("urlsFromMain", results)
 
   } catch (err) {
     console.log(err);
@@ -259,15 +216,12 @@ ipcMain.on('queryTestToMain', async (event, arg) => {
     const responseTime = await checkResponseTime(arg.query, arg.url);
     const queryId = await checkIfQueryExist(arg.query, arg.urlID, arg.name);
   
-
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Insert response time with query_id into database 
     const insertResponseTime = {
       text: 'INSERT INTO response_times(response_time, query_id, date) VALUES ($1, $2, $3)',
       values: [responseTime, queryId, new Date()]
     };
     await db.query(insertResponseTime);
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
   
   
     const getResponseTimes = {
@@ -276,12 +230,9 @@ ipcMain.on('queryTestToMain', async (event, arg) => {
     };
     const responseTimesQueryResults = await db.query(getResponseTimes);
     const responseTimes = responseTimesQueryResults.rows;
-    //Send back array of response times for that query
+
     event.sender.send('responseTimesFromMain', responseTimes);
 
-
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Once new query is in database, send updated state to Test-Query so the new query name is reflected in the dropdown
     const getQueriesQuery = {
       text: 'SELECT _id, query_string, query_name FROM queries WHERE url_id = $1',
       values: [arg.urlID]
@@ -289,7 +240,7 @@ ipcMain.on('queryTestToMain', async (event, arg) => {
     const queryResult = await db.query(getQueriesQuery);
     const allQueries = queryResult.rows;
     event.sender.send('queriesFromMain', allQueries);
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 
   } catch (err) {
@@ -301,26 +252,21 @@ ipcMain.on('queryTestToMain', async (event, arg) => {
 //! #4 loadTestQueryToMain - User selects a query to load test 
 ipcMain.on('loadTestQueryToMain', async (event, arg) => {
   try {
-    // Conduct load test
+
     const loadTestResults = await loadTest(
       arg.numOfChildProccesses,
       arg.url,
       arg.query,
     );
 
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-    //save load test results in db associating it to a specific query
+
     const queryId = await checkIfQueryExist(arg.query, arg.urlID, arg.loadTestQueryName);
     const insertLoadTestResults = {
-      //create new row in load test response time table (date, number_of_child_processes, average_response_time, result, query_id)
       text: 'INSERT INTO load_test_response_times (date, number_of_child_processes, average_response_time, result, query_id) VALUES($1, $2, $3, $4, $5)',
       values: [new Date(), arg.numOfChildProccesses, loadTestResults.averageResponseTime, loadTestResults.successOrFailure, queryId]
     };
     await db.query(insertLoadTestResults);
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-
-    // Once the load test result is inserted, we need the application to receive the new list of updated queries
     const getQueriesQuery = {
       text: 'SELECT _id, query_string, query_name FROM queries WHERE url_id = $1',
       values: [arg.urlID]
@@ -330,8 +276,6 @@ ipcMain.on('loadTestQueryToMain', async (event, arg) => {
     event.sender.send('queriesFromMain', allQueries);
 
 
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
-    // Get allLoadTestResults from database and send all load test results to LoadTest.jsx
     const selectAllLoadTestResults = {
       text: 'SELECT * FROM load_test_response_times WHERE query_id = $1',
       values: [queryId]
@@ -339,7 +283,7 @@ ipcMain.on('loadTestQueryToMain', async (event, arg) => {
     const result = await db.query(selectAllLoadTestResults);
     const allLoadTestResults = result.rows;
     event.sender.send('loadTestResultsFromMain', allLoadTestResults);
-    ///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 
 
   } catch (err) {
@@ -416,19 +360,19 @@ ipcMain.on('dashboardToMain', async (event, arg) => {
   const all_queries = result6.rows;
   const all_load_tests = result7.rows;
 
-  // aggregate all response times by date
+
   const allQueriesRan = {};
   all_queries.forEach((query, index) => {
     const date = new Date(query.date).toDateString();
     allQueriesRan[date] = (allQueriesRan[date] || 0) + 1;
   });
-  // aggregate all load tests by date
+
   const allLoadTestsRan = {};
   all_load_tests.forEach((query, index) => {
     const date = new Date(query.date).toDateString();
     allLoadTestsRan[date] = (allLoadTestsRan[date] || 0) + 1;
   });
-  // combine all response times and load tests into single obj by date
+
   const queriesAndLoadsPerDay = [];
   Object.keys(allQueriesRan).forEach(el => {
     queriesAndLoadsPerDay.push({
