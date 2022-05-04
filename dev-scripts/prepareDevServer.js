29353ebdfe3b04df0b864bfdb78f975592accee8
Laura <ldllano@gmail.com>
2022-05-04 13:34:56 -0400

    remove comments

--- dev-scripts/prepareDevServer.js
+++ dev-scripts/prepareDevServer.js
@@ -7,19 +7,15 @@ const errorLogFilePath = "./dev-scripts/webpack-dev-server-error.log";
 
 console.log("Preparing webpack development server.");
 
-// Delete the old webpack-dev-server.log if it is present
 try {
   fs.unlinkSync(logFilePath);
 } catch (error) {
-  // Existing webpack-dev-server log file may not exist
 }
 
-// Delete the old webpack-dev-server-error.log if it is present
+
 try {
   fs.unlinkSync(errorLogFilePath);
 } catch (error) {
-  // Existing webpack-dev-server-error log file may not exist
 }
 
-// Start the webpack development server
 exec("npm run dev-server");
